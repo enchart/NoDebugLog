82dbf5f69c746a809ffc5d8a0608eefb801afcab
enchart <77974051+enchart@users.noreply.github.com>
2023-12-18 20:34:44 +0500

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace NoDebugLog;
+
+[BepInPlugin(Guid, Name, Version)]
+public class Plugin : BaseUnityPlugin
+{
+    private const string Guid = "com.enchart.nodebuglog";
+    private const string Name = "NoDebugLog";
+    private const string Version = "1.0.0.0";
+        
+    private void Awake()
+    {
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        Debug.unityLogger.logEnabled = false;
+        Logger.LogWarning("Unity logger is turned off now!");
+    }
+}

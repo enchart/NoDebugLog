a283dc58be2479be940cf0b3dc0e81ce0ec3529a
enchart <77974051+enchart@users.noreply.github.com>
2023-12-18 20:53:45 +0500

    Add config

--- Plugin.cs
+++ Plugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using UnityEngine;
 
 namespace NoDebugLog;
@@ -9,11 +10,28 @@ public class Plugin : BaseUnityPlugin
     private const string Guid = "com.enchart.nodebuglog";
     private const string Name = "NoDebugLog";
     private const string Version = "1.0.0.0";
-        
+
+    private readonly ConfigEntry<bool> _configDisableLogging;
+
+    public Plugin()
+    {
+        _configDisableLogging = Config.Bind("General", "DisableLogging", true, "Disables Unity logger.");
+        _configDisableLogging.SettingChanged += (_, _) => LogEnabled = _configDisableLogging.Value;
+    }
+
+    private bool LogEnabled
+    {
+        set
+        {
+            Debug.unityLogger.logEnabled = !value;
+            if (value)
+                Logger.LogWarning($"Unity logger has been disabled! To enable it, edit the configuration of {Name}");
+        }
+    }
+
     private void Awake()
     {
         Logger.LogInfo($"Plugin {Guid} is loaded!");
-        Debug.unityLogger.logEnabled = false;
-        Logger.LogWarning("Unity logger is turned off now!");
+        LogEnabled = _configDisableLogging.Value;
     }
 }
